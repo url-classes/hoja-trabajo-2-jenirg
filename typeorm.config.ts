baefe403cdbead7de9f6749e871dbb0ba2456b89
jenirg <jenierrabanales99@gmail.com>
2024-03-05 11:00:35 -0600

    Creacion de typeorm.config.ts

---
+++ typeorm.config.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+export const dataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'url',
+    password: 'url.2024',
+    database: 'hoja2',
+    synchronize: false,
+    entities: ['src/**/*.entity{.ts, .js}'],
+    migrations: ['./src/migrations/*.ts'],
+});
